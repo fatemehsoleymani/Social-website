6c0424c42f9be037788e635c316363b12af2927b
fatemeh <fatemehsoleymani90@yahoo.com>
2023-06-20 21:16:37 +0430

    register link in login page

--- account/urls.py
+++ account/urls.py
@@ -20,4 +20,4 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
-]
+ ]
