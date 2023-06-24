80339312ba26e77e0aae0c7072db9c1e2226401e
fatemeh <fatemehsoleymani90@yahoo.com>
2023-06-24 14:00:47 +0430

    fix some bugs

--- account/authentication.py
+++ account/authentication.py
@@ -3,9 +3,8 @@ from django.contrib.auth.models import User
 
 class EmailAuthBackend:
     """
- Authenticate using an e-mail address.
-   """
-
+     Authenticate using an e-mail address.
+     """
     def authenticate(self, request, username=None, password=None):
         try:
             user = User.objects.get(email=username)
@@ -17,10 +16,7 @@ class EmailAuthBackend:
 
     def get_user(self, user_id):
         try:
-          return User.objects.get(pk=user_id)
+            return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
-
-
-
 
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -129,6 +129,6 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTHENTICATION_BACKENDS = [
- 'django.contrib.auth.backends.ModelBackend',
- 'account.authentication.EmailAuthBackend',
+     'django.contrib.auth.backends.ModelBackend',
+     'account.authentication.EmailAuthBackend',
 ]
