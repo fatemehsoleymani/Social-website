ab94d6cde7ed2b5295964df8128507eb62115c78
fatemeh <fatemehsoleymani90@yahoo.com>
2023-09-10 00:40:01 +0430

    'add-action-model'

--- images/views.py
+++ images/views.py
@@ -9,6 +9,7 @@ from django.views.decorators.http import require_POST
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, \
  PageNotAnInteger
+from actions.utils import create_action
 
 
 @login_required
@@ -23,6 +24,7 @@ def image_create(request):
             # assign current user to the item
             new_image.user = request.user
             new_image.save()
+            create_action(request.user, 'bookmarked image', new_image)
             messages.success(request,
                              'Image added successfully')
             # redirect to new created item detail view
@@ -54,6 +56,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status': 'ok'})
