a2875f27958dbc551269ed78841fbd1a5f94ac7e
fatemeh <fatemehsoleymani90@yahoo.com>
2023-09-12 20:36:36 +0430

    install docker and redis

--- images/models.py
+++ images/models.py
@@ -37,4 +37,4 @@ class Image(models.Model):
 
     def get_absolute_url(self):
         return reverse('images:detail', args=[self.id,
-                                                self.slug])
+                                              self.slug])
